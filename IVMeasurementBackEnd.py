680afd4ceb067db6a6818ddcafaf14626d851b66
Junho Kim <junhokim@Junho.local>
2024-02-28 22:47:46 +0900

    IVMeasurementBackEnd first version

--- IVMeasurementBackEnd.py
+++ IVMeasurementBackEnd.py
@@ -1,9 +1,11 @@
 from drivers.Keithley2400 import Keithley2400
 from drivers.Keithley6487 import Keithley6487
+import os
 import signal
 import numpy as np
 import time
 import threading
+from util import mkdir, getdate
 
 
 class BaseThread(threading.Thread):
@@ -39,12 +41,21 @@ class IVMeasurementBackend:
 
         self.output_arr = []
 
+        self.out_txt_header = 'Vsmu(V)\tIsmu(A)\tIpau(A)'
+        self.base_path = r'C:\LGAD_test\I-V_test'
+        self.date = ''
         self.out_dir_path = ''
+
+    def _make_out_dir(self):
+        self.date = getdate()
+        self.out_dir_path = os.path.join(self.base_path, f'{self.date}_{self.sensor_name}')
+        mkdir(self.out_dir_path)
 
     def initialize_measurement(self):
 
         self.data_points = -1
         self.output_arr.clear()
+        self.make_out_dir()
 
         self.smu.open(self.smu_address)
         self.smu.initialize()
@@ -87,6 +98,17 @@ class IVMeasurementBackend:
         self.n_measurement_points = len(voltage_array)
         return voltage_array
 
+    def _measure(self, voltage_array):
+        for voltage in voltage_array:
+            self.smu.set_voltage(voltage)
+            voltage_smu, current_smu = self.smu.read().split(',')
+            current_pau, _, _ = self.pau.read().split(',')
+            voltage_smu = float(voltage_smu)
+            current_smu = float(current_smu)
+            current_pau = float(current_pau[:-1])
+            print(voltage, voltage_smu, current_smu, current_pau)  # TODO use verbose level
+            self.output_arr.append([voltage, voltage_smu, current_smu, current_pau])
+
     def start_measurement(self):
 
         self.smu.set_current_limit(self.current_compliance)
@@ -99,8 +121,37 @@ class IVMeasurementBackend:
         time.sleep(1)
 
         # if live_plot then use thread to measure else don't use thread
-        # if thread used need to monitor if measurement is finished or not...
-        # use callback function
+        if self.live_plot:
+            measurement_thread = BaseThread(target=self._measure, args=(voltage_array,),
+                                            callback=self.save_results)
+            measurement_thread.start()
 
-    def make_out_dir(self):
-        pass
+        else:
+            self._measure(voltage_array)
+            self.save_results()
+
+    def get_data(self):
+        if len(self.output_arr) == self.n_measurement_points:
+            return None
+        else:
+            return self.output_arr
+
+    def get_out_dir(self):
+        return self.out_dir_path
+
+    def save_results(self):
+        self.smu.set_voltage(0)
+        self.smu.set_output('off')
+        self.smu.close()
+        self.pau.close()
+
+        file_name = (f'IV_SMU+PAU_{self.sensor_name}_{self.date}_{self.initial_voltage}_{self.final_voltage}'
+                     f'_pad{self.pad_number}')
+        out_file_name = os.path.join(self.out_dir_path, file_name)
+
+        uniq = 1
+        while os.path.exists(out_file_name + '.txt'):
+            uniq += 1
+            out_file_name = f'{out_file_name}_{uniq}'
+
+        np.savetxt(out_file_name + '.txt', self.output_arr, header=self.out_txt_header)
